f1db992e648efabf4a04815703f60037013dfaa4
shrij <shrijuregmi@gmail.com>
2023-07-18 11:26:59 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Campusx")
+
+col1, col2 =  st.columns(2)
+
+with col1:
+    st.image("tulips.jpg")
+with col2:
+    """
+    Campus x is a platform to learn data science. It offers one of the best courses with lots of hands-on projects.
+    """
+
