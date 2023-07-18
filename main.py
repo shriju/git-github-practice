bccb8f3ae058f41dfbb4636eb6903a48d2c3fd51
shrij <shrijuregmi@gmail.com>
2023-07-18 18:37:24 -0400

    replace headder image

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.title("Campusx")
 col1, col2 =  st.columns(2)
 
 with col1:
-    st.image("tulips.jpg")
+    st.image("sunflower.jpg")
 with col2:
     """
     Campus x is a platform to learn data science. It offers one of the best courses with lots of hands-on projects.
